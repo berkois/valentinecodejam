5905c07499ebe37d5c4637e972036799ed99cf77
Israel Berko <berkois@gmail.com>
2022-02-11 02:26:24 +0200

    feat: designed page that gets number and password

---
+++ scripts/index.js
@@ -0,0 +1,61 @@
+const welcomeWindow = document.querySelector(".welcome");
+const welcomeMessage = welcomeWindow.querySelector(".welcome__message");
+const welcomeForm = welcomeWindow.querySelector(".welcome__form_type_welcome");
+const passwordForm = welcomeWindow.querySelector(".welcome__form_type_password");
+const formVisibilityClass = "welcome__form_visible";
+const nameInput = welcomeForm.querySelector(".welcome__input");
+const passwordInput = passwordForm.querySelector(".welcome__input");
+const welcomeGoButton = welcomeWindow.querySelector(".welcome__go-button");
+const correctPassword = "9194";
+
+const isCorrectPassword = (input) => {
+  if (input === correctPassword) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const showGoButton = () => {
+  welcomeGoButton.classList.add("welcome__go-button_active");
+  welcomeGoButton.style.animationPlayState = "running";
+};
+
+const showWelcomeMessage = (givenNumber) => {
+  welcomeMessage.textContent = `The message will be sent to ${givenNumber}.are you good to go?`;
+  welcomeMessage.classList.add("welcome__message_active");
+};
+
+const makeFormVisible = (form) => {
+  form.classList.add(formVisibilityClass);
+};
+
+const makeFormInvisible = (form) => {
+  form.classList.remove(formVisibilityClass);
+};
+
+document.addEventListener("click", () => {
+  welcomeForm.classList.add("welcome__form_no_animation");
+});
+
+welcomeForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  makeFormInvisible(welcomeForm);
+  makeFormVisible(passwordForm);
+});
+
+passwordForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const userName = nameInput.value;
+  const passwordEntered = passwordInput.value;
+  if (isCorrectPassword(passwordEntered)) {
+    makeFormInvisible(passwordForm);
+    showWelcomeMessage(userName);
+    showGoButton();
+  } else {
+    showInputError(passwordForm, passwordInput, { inputErrorClass: "welcome__input_state_error", errorClass: "welcome__input-error_active" });
+    alert("Your love made you confused... Try again.");
+  }
+});
+
+makeFormVisible(welcomeForm);
