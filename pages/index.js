ac3d7de6f26d06e51c66fbc0e41e99a9ff04db35
kaplandavid992 <kaplandavid992@gmail.com>
2022-02-20 17:15:25 +0200

    Merge branch 'main' into david-dates-data

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-import { dates } from "../utils/data.js";
-import RandomDates from "../components/RandomDates.js";
-
-const newRandomDates = new RandomDates(dates, {
-  spinnerButton: ".spinner__button",
-  spinnerContainer: ".spinner__items-container",
-});
-newRandomDates.initItems();
